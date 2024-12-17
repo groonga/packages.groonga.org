2a870724460ec3286b3fca928a4c4baa5d84faff
Sutou Kouhei <kou@clear-code.com>
2024-12-17 17:13:33 +0900

    deployer: Add SourceArchive

---
+++ ansible/files/home/deployer/webhook/lib/deployer/github-client.rb
@@ -0,0 +1,37 @@
+# Copyright (C) 2024  Sutou Kouhei <kou@clear-code.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+require "json"
+require "open-uri"
+
+module Deployer
+  class GitHubClient
+    def initialize(owner, repository)
+      @owner = owner
+      @repository = repository
+    end
+
+    def release(tag)
+      api_uri("releases/tags/#{tag}").open do |input|
+        JSON.parse(input.read)
+      end
+    end
+
+    private
+    def api_uri(path)
+      URI("https://api.github.com/repos/#{@owner}/#{@repository}/#{path}")
+    end
+  end
+end
